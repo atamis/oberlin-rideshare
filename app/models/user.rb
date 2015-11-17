e71b9b56dd1af543462764f8f9fb366c03983a49
Andrew Amis <atamiser@gmail.com>
2015-11-17 13:45:33 -0500

    Email validates for user model
    Added validates to ensure user emails are oberlin emails. Added name to
    admin creation service. Changed default admin email to an oberlin email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,18 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
 
+  validate :oberlin_email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+  def oberlin_email
+    a = email.split("@")
+    if a.length > 1 and a[1] != "oberlin.edu"
+      errors.add(:email, "must be oberlin.edu email")
+    end
+  end
 end
