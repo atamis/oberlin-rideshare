d6e9e26459eb08858a582dfdc0245650455eda3c
Andrew Amis <atamiser@gmail.com>
2015-11-24 14:24:39 -0500

    Made listing_type an enum.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ActiveRecord::Base
+  enum listing_type: [:request, :offer]
   belongs_to :depart_location, class_name: "GeneralLocation",
     foreign_key: :depart_loc_id
   belongs_to :dest_location, class_name: "GeneralLocation",
