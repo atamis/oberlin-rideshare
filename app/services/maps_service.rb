c61c9f6b27a954d46ce5d773bf916e6713793f75
Aakash Patel <contact@aaka.sh>
2015-11-24 16:14:34 -0800

    Added timing debug info for GMaps requests

--- app/services/maps_service.rb
+++ app/services/maps_service.rb
@@ -15,13 +15,14 @@ class MapsService
     end
     
      puts "\tQuerying Google Maps API for address of place_ID: " + place_id
+     request_start_time = Time.new
      response = JSON.parse(
      		     open("https://maps.googleapis.com/maps/api/geocode/json?place_id=" +
                      place_id +
                      "&key=" + @secret).read)
-
-     puts "\tGot response for query of place_ID: " + place_id
-
+     request_duration = Time.new - request_start_time
+     puts "\tGot response for query of place_ID: " + place_id + " (took: " + request_duration.inspect + " secs)"
+    
      if response["status"] == "OK"
         return response["results"].first["formatted_address"]
      else
