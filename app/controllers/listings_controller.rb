dae4b6a11b4a96da603fb60941fe2781a6bc13b7
Aakash Patel <contact@aaka.sh>
2015-11-26 10:19:29 -0500

    user_id needed to be converted to string before displaying

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -101,7 +101,7 @@ class ListingsController < ApplicationController
             puts "detoured_travel_time: " + detoured_travel_time.to_s
             
             if ((detoured_travel_time - direct_travel_time) > detour_time*60)
-	       listing.user_id = "*** (+" + ((detoured_travel_time - direct_travel_time)/60).to_s + " mins) *** + " + listing.user_id
+	       listing.user_id = "*** (+" + ((detoured_travel_time - direct_travel_time)/60).to_s + " mins) *** + " + listing.user_id.to_s
             end 
          end
      else
