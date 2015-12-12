324171054e71fe79ddca1dcc7b3e8e9bccc7c02d
Andrew Amis <atamiser@gmail.com>
2015-12-12 16:00:56 -0500

    Made messages partial, cleaned up messages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     resources :ride_requests do
       get '/accept', to: "ride_requests#accept", as: :accept
       get '/reject', to: "ride_requests#reject", as: :reject
-      resources :messages
+
+      #resources :messages
+      match 'messages/:id' => 'messages#index', via: :get, as: :message
+      match 'messages/' => 'messages#create',  via: :post
+      match 'messages/:id' => 'messages#destroy', via: :delete
     end
   end
   root to: 'visitors#index'
