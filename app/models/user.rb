1adbf9e15a30e7e6460b26829284e178d5727654
Andrew Amis <atamiser@gmail.com>
2015-12-13 13:30:37 -0800

    Added booking requests to the user page.
    Request logic can be found in the mode.

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,17 @@ class User < ActiveRecord::Base
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def listing_requests
+    listings.inject([]) do |acc, l|
+      rs = l.ride_requests
+      if not rs.empty?
+        acc + rs
+      else
+        acc
+      end
+    end
+  end
+
   private
   def oberlin_email
     a = email.split("@")
