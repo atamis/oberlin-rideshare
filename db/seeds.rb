534f322ebc6c02714b3858ebe16b173d1e4cb508
Aakash Patel <contact@aaka.sh>
2015-12-18 22:41:46 -0500

    Got rid of default stuff in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
-
-["Grocery Store", "Liquor Store", "Pharmacy"].each do |name|
-  puts "Making general location " + name
-  GeneralLocation.new(name: name).save!
-end
-
-l = Listing.new
-l.user = user
-l.detour_time = 60
-l.depart_maps_id = "ChIJ5fKhn34KOogRZuYN4JEy-to"
-l.dest_maps_id = "ChIJ5fKhn34KOogRZuYN4JEy-to"
-l.depart_range_start = DateTime.now
-l.depart_range_end = DateTime.now + 2.hours
-l.dest_range_start = DateTime.now + 4.hours
-l.dest_range_end = DateTime.now + 5.hours
-
-r = l.ride_requests.new
-r.user = user
-r.save! # Default undecided state
