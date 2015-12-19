163051714cda96cc29e8007a42cf9ee07e81957d
Aakash Patel <contact@aaka.sh>
2015-12-19 14:08:04 -0500

    timzeone remove

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Ober
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-   config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = :local
+ #  config.time_zone = 'Eastern Time (US & Canada)'
+ #   config.active_record.default_timezone = :local
   end
 end
